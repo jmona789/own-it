b96b0b30f5d89a944f2ad5e58b616e02e86b92c3
jmona789 <jmona789@gmail.com>
2016-03-18 17:38:37 -0400

    added mongoose to server file and added home route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var bodyparser = require("body-parser");
 var logger = require('morgan');
+var mongoose = require('mongoose');
 var PORT = process.env.PORT || 9001;
 var app = express();
 
@@ -12,6 +13,10 @@ app.use(express.static(__dirname + '/public'));
 //DB connection
 var db = 'mongodb://localhost/ownit';
 mongoose.connect(db)
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/public/index.html');
+});
 
 //Listen
 app.listen(PORT, function() {
