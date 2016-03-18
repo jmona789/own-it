0579c634fc8bc9c109f2293401b464ea34fe93e6
jmona789 <jmona789@gmail.com>
2016-03-18 17:35:55 -0400

    moved files around added to server file and added comments

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+//Packages & port
 var express = require("express");
 var bodyparser = require("body-parser");
+var logger = require('morgan');
 var PORT = process.env.PORT || 9001;
-
 var app = express();
 
+//Use logger and public folder
+app.use(logger('dev'));
+app.use(express.static(__dirname + '/public'));
+
+//DB connection
+var db = 'mongodb://localhost/ownit';
+mongoose.connect(db)
+
+//Listen
 app.listen(PORT, function() {
   console.log("LISTENING ON %s", PORT);
 });
