798871c7bc3aeb9a5f3dd2b3ce7454aa699d96b7
jmona789 <jmona789@gmail.com>
2016-03-20 01:08:08 -0400

    added sweet alert package used it for an error message if the user tries to sell an item without being logged in

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
   $(".modal-trigger").leanModal();
+  $(".addItemDisabledBtn").click(function(){
+    swal({   title: "Error!",   text: "You need to be logged in to sell an item!",   type: "error",   confirmButtonText: "Okay" });
+  });
 });
